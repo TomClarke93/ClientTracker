4098273b73c7c0d7e1eebc7d4a6d095230b75d0d
Tom Clarke <thomas.clarke09@gmail.com>
2021-04-24 17:36:49 +0100

    company routes logic implemented

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 
 //MongoDB & Mongoose Setup
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/clientTracker', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/clientTracker', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to MongoDB")
 });
+
+app.use(express.urlencoded({extended: true}));
 
 //Route Handlers
 const companies = require('./routes/companies')
--- routes/companies.js
+++ routes/companies.js
@@ -2,31 +2,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Companies');
+const Company = require('../models/company');
+
+router.get('/', async (req, res) => {
+    const companies = await Company.find({});
+    res.send(companies);
 })
 
 router.get('/new', (req, res) => {
     res.send('Add new Company');
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const newCompany = new Company(req.body.company);
+    await newCompany.save();
     res.redirect('/companies');
 })
 
-router.get('/:id', (req, res) => {
-    res.send('View Company');
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    const foundCompany = await Company.findById(id);
+    res.send(foundCompany);
 })
 
-router.get('/:id/edit', (req, res) => {
-    res.send('Edit Company');
+router.get('/:id/edit', async (req, res) => {
+    const {id} = req.params;
+    const foundCompany = await Company.findById(id);
+    res.send(foundCompany);
 })
 
-router.patch('/:id', (req, res) => {
-    res.redirect('/:id');
+router.patch('/:id', async (req, res) => {
+    const {id} = req.params;
+    const foundCompany = await Company.findByIdAndUpdate(id, {...req.body.company});
+    res.redirect(`/companies/${id}`);
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    const foundCompany = await Company.findByIdAndDelete(id);
     res.redirect('/companies');
 })
 
