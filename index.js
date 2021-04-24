21bb751104ae92f69d758c4b3ace05fc10ca7f58
Tom Clarke <thomas.clarke09@gmail.com>
2021-04-24 15:32:49 +0100

    mongo & mongoose setup

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+//Express Setup
 const express = require('express');
 const app = express();
+
+//MongoDB & Mongoose Setup
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/clientTracker', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB")
+});
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send("Landing Page")
