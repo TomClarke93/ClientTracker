3f0f719955d4313b857bee0958606f53451905d6
Tom Clarke <thomas.clarke09@gmail.com>
2021-04-25 20:25:52 +0100

    ejs set up, views directory added, boilerplate created

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //Express Setup
 const express = require('express');
 const app = express();
+const path = require('path');
 
-//MongoDB & Mongoose Setup
+const ejsMate = require('ejs-mate');
+
+//MongoDB & Mongoose Configuration
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/clientTracker', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 const db = mongoose.connection;
@@ -11,16 +14,19 @@ db.once('open', function() {
   console.log("Connected to MongoDB")
 });
 
+//Template Engine Configuration
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
 app.use(express.urlencoded({extended: true}));
 
 //Route Handlers
 const companies = require('./routes/companies')
 app.use('/companies', companies);
 
-
-
 app.get('/', (req, res) => {
-    res.send("Landing Page")
+    res.render('home');
 })
 
 app.listen(3000, function(){
